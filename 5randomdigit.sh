0bdbd003fde1ddf14d1eed310b26539ca7436848
unknown <mayurpawar8691@gmail.com>
2022-03-22 20:39:32 +0530

    Added Sequences Problem

---
+++ 5randomdigit.sh
@@ -0,0 +1,15 @@
+a=$(((RANDOM%90)+10))
+echo a=$a
+b=$(((RANDOM%90)+10))
+echo b=$b
+c=$(((RANDOM%90)+10))
+echo c=$c
+d=$(((RANDOM%90)+10))
+echo d=$d
+e=$(((RANDOM%90)+10))
+echo e=$e
+sum=$((a+b+c+d+e))
+echo Sum=$sum
+avg=$(((a+b+c+d+e)/5))
+echo Avg=$avg 
+
